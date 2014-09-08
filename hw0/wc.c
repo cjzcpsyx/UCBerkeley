9f5c775fbf3f67ccf1fb948549b31a4cf197199e
harrychen <harrychen-cjz@hotmail.com>
2014-09-08 12:26:49 -0700

    submit

--- hw0/wc.c
+++ hw0/wc.c
@@ -37,10 +37,10 @@ void wc(FILE *ofile, FILE *infile, char *inname) {
 		printf(" %*d %*d %*d\n", count, line_count, count, word_count, count, char_count);
 	}
 	else if (ofile == NULL) {
-		printf("%d %d %d %s\n", count, line_count, count, word_count, count, char_count, inname);
+		printf("%*d %*d %*d %s\n", count, line_count, count, word_count, count, char_count, inname);
 	}
 	else {
-		fprintf(ofile, "%d %d %d %s\n", line_count, word_count, char_count, inname);
+		fprintf(ofile, "%*d %*d %*d %s\n", count, line_count, count, word_count, count, char_count, inname);
 		fclose(ofile);
 	}
 }
