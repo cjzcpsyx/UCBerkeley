0088bd534e8d3b85748f9c18d9009b81a1ac7389
harrychen <harrychen-cjz@hotmail.com>
2014-09-09 16:05:57 -0700

    Finished adding libc functionality into the shell.

--- hw1/shell.c
+++ hw1/shell.c
@@ -11,6 +11,8 @@
 #include "io.h"
 #include "parse.h"
 
+char *path = ".";
+
 int cmd_quit(tok_t arg[]) {
   printf("Bye\n");
   exit(0);
@@ -18,6 +20,8 @@ int cmd_quit(tok_t arg[]) {
 }
 
 int cmd_help(tok_t arg[]);
+
+int cmd_cd(tok_t arg[]);
 
 /* Command Lookup table */
 typedef int cmd_fun_t (tok_t args[]); /* cmd functions take token arra and rtn int */
@@ -29,7 +33,8 @@ typedef struct fun_desc {
 
 fun_desc_t cmd_table[] = {
   {cmd_help, "?", "show this help menu"},
-  {cmd_quit, "quit", "quit the command shell"}
+  {cmd_quit, "quit", "quit the command shell"},
+  {cmd_cd, "cd", "change directory"}
 };
 
 int cmd_help(tok_t arg[]) {
@@ -38,6 +43,28 @@ int cmd_help(tok_t arg[]) {
     printf("%s - %s\n",cmd_table[i].cmd, cmd_table[i].doc);
   }
   return 1;
+}
+
+int cmd_cd(tok_t arg[]) {
+  int result;
+  result = chdir(arg[0]);
+  if (result == -1) {
+    fprintf(stdout,"%s\n", strerror(errno));
+  }
+  else {
+    path = arg[0];
+  }
+  return 1;
+}
+
+char * cmd_pwd() {
+  long size;
+  char *buf;
+  char *ptr;
+  size = pathconf(path, _PC_PATH_MAX);
+  if ((buf = (char *)malloc((size_t)size)) != NULL)
+      ptr = getcwd(buf, (size_t)size);
+  return ptr;
 }
 
 int lookup(char cmd[]) {
@@ -56,10 +83,10 @@ int shell (int argc, char *argv[]) {
   pid_t pid = getpid();		/* get current processes PID */
   pid_t ppid = getppid();	/* get parents PID */
 
-  printf("%s running as PID %d under %d\n",argv[0],pid,ppid);
+  printf("%s running as PID %d under %d\n",argv[0],pid,ppid);  
 
   lineNum=0;
-  fprintf(stdout,"%d: ",lineNum);
+  fprintf(stdout,"%s[%d]: ", cmd_pwd(), lineNum);
   while ((s = freadln(stdin))) {
     t = getToks(s);		/* Break the line into tokens */
     fundex = lookup(t[0]);	/* Is first token a shell literal */
@@ -67,7 +94,7 @@ int shell (int argc, char *argv[]) {
     else {			/* Treat it as a file to exec */
       fprintf(stdout,"This shell currently supports only built-ins.  Replace this to run programs as commands.\n");
     }
-    fprintf(stdout,"%d: ",++lineNum);
+    fprintf(stdout,"%s[%d]: ", cmd_pwd(), ++lineNum);
   }
   return 0;
 }
