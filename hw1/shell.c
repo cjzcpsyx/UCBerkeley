5752c4c2dca1275ac435a00e3c5b9b9415d2840b
harrychen <harrychen-cjz@hotmail.com>
2014-09-14 23:34:38 -0700

    background added

--- hw1/shell.c
+++ hw1/shell.c
@@ -45,6 +45,13 @@ char * concat(char *s1, char *s2) {
   return new_str;
 }
 
+char * last(tok_t arg[]) {
+  int i;
+  for (i=0; arg[i] != '\0'; i++)
+  ;
+  return arg[--i];
+}
+
 int cmd_help(tok_t arg[]) {
   int i;
   for (i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
@@ -127,11 +134,37 @@ int shell (int argc, char *argv[]) {
   while ((s = freadln(stdin))) {
     t = getToks(s);   /* Break the line into tokens */
     fundex = lookup(t[0]);  /* Is first token a shell literal */
-    if (fundex >= 0) cmd_table[fundex].fun(&t[1]);
-    else {      /* Treat it as a file to exec */
-      cmd_exec(&t[0]);
+    if (strcmp(last(t), "&") == 0) {
+      tok_t *new_tok = malloc(strlen(s)+1);
+      int i;
+      for (i=0; strcmp(t[i], "&") != 0; i++) {
+        new_tok[i] = t[i];
+      }
+      pid_t cpid;
+      int cstatus;
+      cpid = fork();
+      if(cpid < 0 ) {
+        perror("fork failure");
+        exit(1);
+      }
+      if (cpid == 0) {
+        if (fundex >= 0) cmd_table[fundex].fun(&new_tok[1]);
+        else {      /* Treat it as a file to exec */
+          cmd_exec(&new_tok[0]);
+        }
+      }
+      else {
+        waitpid(cpid, &cstatus, 0);
+        fprintf(stdout,"%s[%d]: ", cmd_pwd(), ++lineNum);
+      }
     }
-    fprintf(stdout,"%s[%d]: ", cmd_pwd(), ++lineNum);
+    else {
+      if (fundex >= 0) cmd_table[fundex].fun(&t[1]);
+      else {      /* Treat it as a file to exec */
+        cmd_exec(&t[0]);
+      }
+      fprintf(stdout,"%s[%d]: ", cmd_pwd(), ++lineNum);
+    }
   }
   return 0;
 }
